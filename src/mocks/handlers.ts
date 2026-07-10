1f17810e8c9a6bf36ad08b2101fc5ad9d942a5b0
Igor Parra <igorricardoparra@gmail.com>
2026-07-09 23:29:52 -0300

    chore(1.4): add MSW handlers for clients and transport types

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,11 @@
 import { http, HttpResponse } from "msw";
+import { clientHandlers } from "./handlers/clients";
+import { transportTypeHandlers } from "./handlers/transport-types";
 
 export const handlers = [
   http.get("/api/ping", () => {
     return HttpResponse.json({ message: "pong" });
   }),
+  ...clientHandlers,
+  ...transportTypeHandlers,
 ];
