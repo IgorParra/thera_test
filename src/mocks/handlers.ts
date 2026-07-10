b333363c3465f77cdcf2878bf20d56c352c0b31e
Igor Parra <igorricardoparra@gmail.com>
2026-07-09 23:31:06 -0300

    chore(1.6): add MSW handlers for sales orders with status transitions and audit

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,6 +2,7 @@ import { http, HttpResponse } from "msw";
 import { clientHandlers } from "./handlers/clients";
 import { transportTypeHandlers } from "./handlers/transport-types";
 import { itemHandlers } from "./handlers/items";
+import { orderHandlers } from "./handlers/orders";
 
 export const handlers = [
   http.get("/api/ping", () => {
@@ -10,4 +11,5 @@ export const handlers = [
   ...clientHandlers,
   ...transportTypeHandlers,
   ...itemHandlers,
+  ...orderHandlers,
 ];
